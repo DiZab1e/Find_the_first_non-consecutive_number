b0a9a309fbc01b7b3de90d107f67432b553313da
Gregory <107585413+DiZab1e@users.noreply.github.com>
2022-06-24 00:47:17 +0300

    Add non_cons_number.cs

---
+++ non_cons_number.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Linq;
+
+public class Kata
+{
+    public static object FirstNonConsecutive(int[] arr)
+    {
+        for (int i = 1; i < arr.Length; i++)
+        {
+            if (arr[i] - arr[i - 1] != 1)
+            {
+                return arr[i];
+            }
+        }
+
+        return null;
+    }
+}
